b3b1cfc98aca8fedc780c4372061cf488af158a5
unknown <mr.ankityo111@gmail.com>
2021-08-18 13:18:45 +0530

    first

---
+++ src/com/flipkart/java/App.java
@@ -0,0 +1,10 @@
+package com.flipkart.java;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("My first project");
+	}
+
+}
